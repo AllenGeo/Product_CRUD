f7e4036f6fb38b147dc6a22a390273021d7787cf
AllenGeo <107992314+AllenGeo@users.noreply.github.com>
2023-07-26 17:15:08 +0530

    ProductCrudApplication.java

--- Product_CRUD/src/main/java/com/items/ProductCrudApplication.java
+++ Product_CRUD/src/main/java/com/items/ProductCrudApplication.java
@@ -3,11 +3,16 @@ package com.items;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Product_CRUD API", version = "1.0", description = "Products Information"))
+
 public class ProductCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCrudApplication.class, args);
 	}
-
 }
